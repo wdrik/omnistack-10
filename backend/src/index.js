521c7b631c36bdf031c244ec673ea874c0163d37
Iorgen Wildrik <w.drik@outlook.com>
2020-03-05 20:41:05 -0300

    Added developer registration functionality + enable cors in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -18,6 +19,7 @@ mongoose.connect('mongodb+srv://omnistack:1234@cluster0-nhv9j.mongodb.net/omnist
   useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from "./services/api";
 
 import "./global.css";
 import "./App.css";
@@ -6,31 +8,93 @@ import "./Sidebar.css";
 import "./Main.css";
 
 function App() {
+  const [githubUsername, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    await api.post("/devs", {
+      github_username: githubUsername,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername("");
+    setTechs("");
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
 
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={githubUsername}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
+              <input
+                name="latitude"
+                type="number"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Logitude</label>
-              <input name="longitude" id="longitude" />
+              <input
+                name="longitude"
+                type="number"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
