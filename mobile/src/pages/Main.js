c2be6d82e23dbbec9bc5d13649c17668c297aeff
Iorgen Wildrik <w.drik@outlook.com>
2020-03-08 17:09:20 -0300

    Render MapView with user current location!

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+import { StyleSheet } from "react-native";
+
+import MavView from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+
+export default function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  });
+
+  if (!currentRegion) return null;
+
+  return <MavView initialRegion={currentRegion} style={styles.map} />;
+}
+
+const styles = StyleSheet.create({
+  map: { flex: 1 }
+});
